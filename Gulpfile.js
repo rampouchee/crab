af6e156015b25102ff582f8db4b6dac9684354a8
rampouchee <rampouchee@gmail.com>
2016-01-31 12:01:31 +0100

    v0.1.4 with src

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,23 +2,26 @@ const gulp = require('gulp');
 const postcss = require('gulp-postcss');
 const nano = require('gulp-cssnano');
 const rename = require('gulp-rename');
+const autoprefixer = require('autoprefixer');
+const precss = require('precss');
 
 gulp.task('css', function () {
   return gulp.src('./src/*.css')
     .pipe(
       postcss([
-        require('autoprefixer'),
-        require('precss')
+        autoprefixer({ browsers: ['last 2 versions'] }),
+        precss({})
       ])
-    ).pipe(gulp.dest('./dist/'));
+    )
+    .pipe(gulp.dest('./dist/'));
 });
 
 gulp.task('minify', function () {
   return gulp.src('./src/*.css')
     .pipe(
       postcss([
-        require('autoprefixer'),
-        require('precss')
+        autoprefixer({ browsers: ['last 2 versions'] }),
+        precss({})
       ])
     )
     .pipe(nano())
